3e6666d6a9b7c6897eaf7e031b34222a3bbefcb2
devbycpw <christian.peter.dev@gmail.com>
2025-12-05 23:28:35 +0700

    fix-home

--- app/Views/layouts/Main.php
+++ app/Views/layouts/Main.php
@@ -8,7 +8,7 @@ $role = Session::get('role') ?? 'guest';
     <title><?= $title ?></title>
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
     <?php if(isset($custom_css)): ?>
         <link rel="stylesheet" href="<?= BASE_URL ?>assets/style/<?= $custom_css ?>.css">
@@ -23,6 +23,6 @@ $role = Session::get('role') ?? 'guest';
     </main>
 
     <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
